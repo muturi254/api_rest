cb4caedeb91654999b636f6e424b043ed848fb0e
muturi254 <petermuturi74@gmail.com>
2021-04-27 11:36:03 +0300

    add view for category endpoint

--- e_api/views.py
+++ e_api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import generics
 
+from e_api.models import Category, Book, Product
+from e_api.serializers import ProductSerializer, CategorySerializer, BookSerializer
 # Create your views here.
+class ListCategory(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer

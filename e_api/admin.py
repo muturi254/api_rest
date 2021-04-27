74db249a9bfaa46f4540e015c192c8415b8c0bef
muturi254 <petermuturi74@gmail.com>
2021-04-27 11:16:08 +0300

    add models to admin site register

--- e_api/admin.py
+++ e_api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from e_api.models import Category, Book, Product
+
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Book)

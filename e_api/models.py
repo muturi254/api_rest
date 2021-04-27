f1aeccaa909cce94831903b7b2425c97afdb9160
muturi254 <petermuturi74@gmail.com>
2021-04-27 11:06:51 +0300

    add models to e_api app

--- e_api/models.py
+++ e_api/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=150)
+    category = models.ForeignKey(Category, related_name="books", on_delete=models.CASCADE)
+    isbn = models.CharField(max_length=13)
+    pages = models.IntegerField()
+    price = models.IntegerField()
+    stock = models.IntegerField()
+    description = models.TextField()
+    imageUrl = models.URLField()
+    status = models.BinaryField()
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-date_created']
+
+    def __str__(self):
+        return self.title
+    
+
+class Product(models.Model):
+    product_tag = models.CharField(max_length=10)
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, related_name="products", on_delete=models.CASCADE)
+    price = models.IntegerField()
+    stock = models.IntegerField()
+    description = models.TextField()
+    imageUrl = models.URLField()
+    status = models.BinaryField()
+    date_created = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        ordering = ['-date_created']
+
+    def __str__(self):
+        return f'{0} {1}'.format(self.name, self.product_tag)

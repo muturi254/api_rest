d1b9ecc74dbb9a1fefbdde2879d273fa3d7ccc07
muturi254 <petermuturi74@gmail.com>
2021-05-02 17:59:31 +0300

    add custom manager for users model

---
+++ users/managers.py
@@ -0,0 +1,33 @@
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import ugettext_lazy as _
+
+class CustomUserManager(BaseUserManager):
+    """
+        Create user with unique email and password
+    """
+    def create_user(self, email, password, **extra_fields):
+        """
+            create User with given email and password
+        """
+        if not email:
+            raise ValueError(_('the email must be set'))
+
+        email = self.normalize_email(email)
+        user = self.model(email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        """
+            Create admin user with specified email and passsword
+        """
+        extra_fields.setdefaults('is_staff', True)
+        extra_fields.setdefaults('is_superuser', True)
+        extra_fields.setdefaults('is_active', True)
+
+        if extra_fields.get('is_satff') is not True:
+            raise ValueError(_('superuser must have is_staff=True'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('superuser must have is_superuser=True'))
+        return self.create_user(email, password, **extra_fields)

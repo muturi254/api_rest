1959494a8d603dcf2648423367d0014876cbe720
muturi254 <petermuturi74@gmail.com>
2021-05-02 18:14:19 +0300

    add users app to installed app and register new customUser model as Uth model

--- users/models.py
+++ users/models.py
@@ -1,13 +1,15 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser
 from django.utils.translation import ugettext_lazy as _
 
 from users.managers import CustomUserManager
 
 # Create your models here.
-class CustomUser(AbstractUser):
-    username = None
+class CustomUser(AbstractBaseUser):
     email = models.EmailField(_('email address'), unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ()
